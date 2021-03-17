2f3801641c8d6b7eb7290b642d79637eddae7a41
BTMichalowicz <benjaminmichalowicz98@gmail.com>
2021-03-16 21:30:28 -0400

    Not sure why I'm getting these unsued variable issues

--- src/topylogic.c
+++ src/topylogic.c
@@ -10,7 +10,7 @@ int edge_f(void *args, void *glbl, const void *const edge_vars_a, const void *co
     //Make the callback to py_callback happen
     //Rinse and repeat for vertex_f and generic_f
 
-		if(!((PyCFunction)(int)(*)(void*, void* const void* const, const void* const)py_callback(args, glbl2, edge_vars_a, edge_vars_b))) return -1; 
+		if(!((PyCFunction)(int(*)(void*, void*, const void* const, const void* const)py_callback(args, glbl2, edge_vars_a, edge_vars_b)))) return -1; 
 		return 0;
 }
 
@@ -18,10 +18,10 @@ int edge_f(void *args, void *glbl, const void *const edge_vars_a, const void *co
 void vertex_f(struct graph* graph, struct vertex_result* args, void* glbl, void* edge_vars){
 	struct glbl_args* g = (struct glbl_args *) glbl;
 	PyObject *pyCallback = g->py_callback;
-	void * glbl2 = glbl_args->glbl;
+	void * glbl2 = g->glbl;
 	
-	(PyCFunction)(void)(*)(struct graph*, struct vertex_result*, void* glbl, void* edge_vars)pyCallback(graph, args, glbl2, edge_vars);
-
+	void* res = (PyCFunction)(void(*)(struct graph*, struct vertex_result*, void*, void*)(pyCallback(graph, args, glbl2, edge_vars)));
+	assert(res!=NULL);
 }
 
 
@@ -31,7 +31,9 @@ void generic_f(void* glbl){
 	PyObject *pyCallback = g->py_callback;
 	void* glbl2 = g->glbl;
 
-	(PyCFunction)(void)(*)(void*)pyCallback(glbl2);
+	void* res = (PyCFunction)(void(*)(void*)(pyCallback(glbl2)));
+
+	assert(res!=NULL);
 
 
 }
